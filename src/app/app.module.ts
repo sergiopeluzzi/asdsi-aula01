b8c57dc3b81e54313fd29e0b16bec363fd1604dd
Sergio Peluzzi <semz@gft.com>
2023-09-03 00:21:10 -0300

    projeto inicial adicionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BotaoComponent } from './components/botao/botao.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, BotaoComponent, HeaderComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/botao/botao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botao',
+  templateUrl: './botao.component.html',
+  styleUrls: ['./botao.component.css']
+})
+export class BotaoComponent {
+
+}
